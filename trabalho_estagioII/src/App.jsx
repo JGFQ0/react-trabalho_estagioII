032401779ccc8c957f03f732d256b7e4f31b08e1
Gabriel <gaabrielquueiiroz@gmail.com>
2024-12-03 12:58:24 -0300

    Página 2 adicionada

--- trabalho_estagioII/src/App.jsx
+++ trabalho_estagioII/src/App.jsx
@@ -1,13 +1,47 @@
 import './App.css'
-import ConteudoPag1 from './components/ConteudoPag1'
+import BtnAgendar from './components/BtnAgendar'
 
-
-function App() {
+export default function App() {
   return (
     <>
-        <ConteudoPag1></ConteudoPag1>
+        <div className="container1">
+        <h1>Agendamento de Quadra Esportiva</h1>
+        <form id="agendForm">
+            <fieldset>
+                <div className="row">
+                    <div className="column">
+                        <label htmlFor="nome">Nome:</label>
+                        <input className="opções" type="text" id="nome" name="nome" placeholder="Escreva seu nome..." required/>
+                    </div>
+                    <div className="column">
+                        <label htmlFor="esporte">Esportes </label>
+                        <select className="opções" id="esporte" name="esporte" required>
+                            <option value="Futebol">Futebol</option>
+                            <option value="Vôlei">Vôlei</option>
+                        </select>
+                    </div>
+                </div>
+                
+                <div className="row">
+                    <div className="column">
+                        <label htmlFor="data">Data:</label>
+                        <input className="opções" type="date" id="data" name="data" required/>
+                    </div>
+                    <div className="column">
+                        <label htmlFor="horario">Horários Disponíveis:</label>
+                        <select className="opções" id="horario" name="horario" required>
+                                <option value="08:00">08:00</option>
+                                <option value="09:00">09:00</option>
+                                <option value="16:00">16:00</option>
+                                <option value="17:00">17:00</option>
+                        </select>
+                    </div>
+                </div>
+            <BtnAgendar/>
+            <a href='Page2.html'><button className="botões" type="button">Ver Agendamentos {'>>>'}</button></a>
+            </fieldset>
+        </form>
+        </div>
     </>
   )
 }
-
-export default App
